d776d195ffeb2f582730efc4f95bc6ccc1b5fd2a
danzrou <droujinsky@gmail.com>
2015-11-11 14:56:11 +0200

    Exercise 03

---
+++ src/com/danz/javacourse/Calculator.java
@@ -0,0 +1,33 @@
+package com.danz.javacourse;
+
+public class Calculator{
+	private double circleArea;
+	private double triOpposite;
+	
+	public void calculateCircle(int radius){
+		circleArea = Math.pow(radius, 2.0) * Math.PI;
+	}
+	
+	public double getCircleArea(){
+		return circleArea;
+	}
+	
+	public void calculateTriOpp(double degree, double hypotenuse){
+		triOpposite = Math.sin(Math.toRadians(degree)) * hypotenuse;
+	}
+	
+	public double getTriOpp(){
+		return triOpposite;
+	}
+	
+	public void main(String[] args){
+		Calculator circle, triangle;
+		circle = new Calculator();
+		triangle = new Calculator();
+		
+		circle.calculateCircle(50);
+		triangle.calculateTriOpp(30, 50);
+		
+		
+	}
+}
--- src/com/danz/javacourse/MTA_ProjectServlet.java
+++ src/com/danz/javacourse/MTA_ProjectServlet.java
@@ -2,18 +2,30 @@
 package com.danz.javacourse;
 
 import java.io.IOException;
-import java.util.Scanner;
+import java.text.DecimalFormat;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 @SuppressWarnings("serial")
 public class MTA_ProjectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world!!!!!!!!!!!!!!!!!!! :)))))");
+		resp.getWriter().println("Application successfully loaded");
+		
+		Calculator circle = new Calculator();
+		Calculator triangle = new Calculator();
+		
+		DecimalFormat df = new DecimalFormat("0.000");
+		
+		circle.calculateCircle(50);
+		triangle.calculateTriOpp(30, 50);
+		resp.getWriter().println("Circle Area is " + df.format(circle.getCircleArea()) +
+								 "\n" +
+								 "Triangle opposite is " + df.format(triangle.getTriOpp()));
 	}
 }
 
