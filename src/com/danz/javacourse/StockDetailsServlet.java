92fbef81c3836e8af6226e3cccf763377700cec7
danzrou <droujinsky@gmail.com>
2015-11-18 15:26:07 +0200

    Exercise 04

--- src/com/danz/javacourse/StockDetailsServlet.java
+++ src/com/danz/javacourse/StockDetailsServlet.java
@@ -11,13 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 public class StockDetailsServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-				
+			
+		resp.setContentType("text/plain");
 		Stock stock1 = new Stock("PIH", 13.1f, 12.4f, new Date("11/15/2015"));
 		Stock stock2 = new Stock("AAL", 5.78f, 5.5f, new Date("11/15/2015"));
 		Stock stock3 = new Stock("CAAS", 32.2f, 31.5f, new Date("11/14/2015"));
 
-		resp.getWriter().println(
-				"Stock" + stock1.getHtmlDescription() + "\n" +
+		resp.getWriter().print(
+				"Stock" + stock1.getHtmlDescription() + "\n" +  
 				"Stock"	+ stock2.getHtmlDescription() + "\n" +
 				"Stock" + stock3.getHtmlDescription()
 				);
