1009370ec86a6f69dacff67635fc7619da1d0849
danzrou <droujinsky@gmail.com>
2015-11-18 11:18:28 +0200

    Exercise 03 - Last V

--- src/com/danz/javacourse/Exercise03.java
+++ src/com/danz/javacourse/Exercise03.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*
+ * Ziv is a douchebag
+ */
+
 @SuppressWarnings("serial")
 public class Exercise03 extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
