f0b553b4be169f49b0d2a2801506fb3035b1765a
danzrou <droujinsky@gmail.com>
2015-12-09 13:22:55 +0200

    Exercise 06 - Javadoc and Updated Portfolio Manager
    Signed-off-by: danzrou <droujinsky@gmail.com>

--- src/com/danz/javacourse/service/PortfolioManager.java
+++ src/com/danz/javacourse/service/PortfolioManager.java
@@ -1,6 +1,6 @@
 package com.danz.javacourse.service;
 
-import java.util.Date;
+//import java.util.Date;
 
 import com.danz.javacourse.model.Portfolio;
 import com.danz.javacourse.model.Stock;
@@ -8,11 +8,15 @@ import com.danz.javacourse.model.Stock;
 public class PortfolioManager {
 	private Portfolio portfolio = new Portfolio();
 	
-	@SuppressWarnings("deprecation")
+	//@SuppressWarnings("deprecation")
+	public void addStocksToPortfolio(Stock stock){
+		portfolio.addStocks(stock);
+	}
+	
 	public Portfolio getPortfolio(){
-		this.portfolio.addStocks(new Stock("PIH", 13.1f, 12.4f, new Date("11/15/2015")));
-		this.portfolio.addStocks(new Stock("AAL", 5.78f, 5.5f, new Date("11/15/2015")));
-		this.portfolio.addStocks(new Stock("CAAS", 32.2f, 31.5f, new Date("11/14/2015")));
+//		this.portfolio.addStocks(new Stock("PIH", 13.1f, 12.4f, new Date("11/15/2015")));
+//		this.portfolio.addStocks(new Stock("AAL", 5.78f, 5.5f, new Date("11/15/2015")));
+//		this.portfolio.addStocks(new Stock("CAAS", 32.2f, 31.5f, new Date("11/14/2015")));
 		return portfolio;
 	}
 }
--- src/com/danz/javacourse/servlet/PortfolioServlet.java
+++ src/com/danz/javacourse/servlet/PortfolioServlet.java
@@ -1,37 +1,42 @@
 package com.danz.javacourse.servlet;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.danz.javacourse.model.Portfolio;
+import com.danz.javacourse.model.Stock;
 import com.danz.javacourse.service.PortfolioManager;
 
-@SuppressWarnings("serial")
+@SuppressWarnings({"serial", "deprecation"})
 public class PortfolioServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/html");
 		PortfolioManager portfolioManager = new PortfolioManager();
+		portfolioManager.addStocksToPortfolio(new Stock("PIH", 13.1f, 12.4f, new Date("11/15/2015")));
+		portfolioManager.addStocksToPortfolio(new Stock("AAL", 5.78f, 5.5f, new Date("11/15/2015")));
+		portfolioManager.addStocksToPortfolio(new Stock("CAAS", 32.2f, 31.5f, new Date("11/14/2015")));
+		
 		Portfolio portfolio1 = portfolioManager.getPortfolio();
 		portfolio1.setTitle("<h1> Portfolio #1</h1>");
-		
 		Portfolio portfolio2 = new Portfolio(portfolio1); // Exercise 06
 		portfolio2.setTitle("<h1> Portfolio #2</h1>");
 		
 		resp.getWriter().println(portfolio1.getHtmlString() + 
-								portfolio2.getHtmlString());
+								 portfolio2.getHtmlString());
 		
 		portfolio1.deleteFromStockList(0);
 		
-		resp.getWriter().println(
-				portfolio1.getHtmlString() + portfolio2.getHtmlString());
+		resp.getWriter().println(portfolio1.getHtmlString() + 
+				                 portfolio2.getHtmlString());
 		
 		portfolio2.getStocks().get(2).setBid(55.55f);
 		
-		resp.getWriter().println(
-				portfolio1.getHtmlString() + portfolio2.getHtmlString());
+		resp.getWriter().println(portfolio1.getHtmlString() + 
+								 portfolio2.getHtmlString());
 	}
 }
